835b661a74ae8149038cf133981692e07f757947
Artūrs Grebstelis <arturs@mikrotik.com>
2024-09-09 14:27:04 +0300

    ppu wipe screen to black

--- katango.c
+++ katango.c
@@ -35,9 +35,35 @@ void setup_palette(void) {
     }
 }
 
+static volatile word ppu_addr;
+static volatile byte ppu_count;
+static volatile byte ppu_buffer[32];
+
 void irq_handler(void) {
+    byte i;
+    PPUADDR(ppu_addr);
+    for (i = 0; i < ppu_count; i++) {
+	PPUDATA(ppu_buffer[i]);
+    }
+    ppu_count = 0;
+}
+
+static void ppu_update_row(void) {
+    ppu_count = 32;
+    while (ppu_count > 0) { }
+    ppu_addr += 32;
+}
+
+static void wipe_screen(void) {
+    byte i;
+    ppu_addr = 0x2020;
+    for (i = 0; i < 32; i++) {
+	ppu_buffer[i] = 0;
+    }
+    for (i = 0; i < 28; i++) {
+	ppu_update_row();
+    }
 }
 
 void game_startup(void) {
 }
-
